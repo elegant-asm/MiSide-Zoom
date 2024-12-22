8c4405f06d89f90b20ecb65216e6017e2f9f62e9
redacted <eazyzero.yt@gmail.com>
2024-12-22 17:56:49 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Zoom
+Gives permission to zoom in-out everywhere.
+
+`Right Mouse Button - Zoom in-out`
+
+`ALT + Mouse Scroll Up - Zoom increase`
+
+`ALT + Mouse Scroll Down - Zoom decrease`
+
+# Requires BepInEx 6.0.0
+You need to install bepinex to root game folder from bepis builds.
+
+**IT GAME REQUIRES BepInEx.Unity.Il2Cpp.x64-win**!!!
+
+https://builds.bepinex.dev/projects/bepinex_be
+> Tested on BepInEx 6.0.0 be.729+35f6b1b.
+
+# How to Install BepInEx & plugins
+https://youtu.be/kA1J2K4pcjg
---
+++ src/Plugin.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Zoom;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static ManualLogSource Log;
+
+    internal static Harmony harmony = new Harmony("zoom");
+
+    public override void Load()
+    {
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.DEBUG = true;
+
+        harmony.PatchAll(typeof(Patch_WorldPlayer));
+    }
+
+    internal static class Patch_WorldPlayer
+    {
+		private static readonly float maxZoom = 10f;
+        private static readonly float minZoom = 70f;
+		private static readonly float zoomIncrement = 2f;
+        private static float zoom = 20f;
+		private static bool zooming = false;
+
+		[HarmonyPatch(typeof(WorldPlayer), "Update")]
+		[HarmonyPrefix]
+		public static void Update(WorldPlayer __instance) {
+			if (Input.GetKey(KeyCode.LeftAlt)) {
+				if (Input.mouseScrollDelta.y > 0f) {
+					float newZoom = zoom - zoomIncrement;
+					if (newZoom >= maxZoom) {
+                        zoom = newZoom;
+                    } else if (zoom != maxZoom) {
+                        zoom = maxZoom;
+                    }
+                } else if (Input.mouseScrollDelta.y < 0f) {
+					float newZoom = zoom + zoomIncrement;
+					if (newZoom <= minZoom) {
+                        zoom = newZoom;
+                    } else if (zoom != minZoom) {
+                        zoom = minZoom;
+                    }
+                }
+                if (zooming) {
+                    __instance.CameraFOVSharply(zoom);
+                }
+            }
+			if (Input.GetKeyDown(KeyCode.Mouse1)) {
+				zooming = true;
+                __instance.CameraAnimationFOV(zoom);
+            }
+            if (!Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.Mouse1) && zooming) {
+				zooming = false;
+                __instance.CameraFOVLerpReset();
+            }
+		}
+	}
+}
